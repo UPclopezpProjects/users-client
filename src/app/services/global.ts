70edc4fac7d605018965b876a98b592ed4462602
Oznnniv <>
2021-04-29 19:21:43 -0500

    Upgrades

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -2,9 +2,3 @@ export var GLOBAL = {
 	url: 'http://localhost:80/',
 	ip: '0.0.0.0'
 };
-/*VARIABLE lOCAL
-export var GLOBAL = {
-	url: 'http://host.docker.internal:5000/',
-	ip: 'host.docker.internal'
-};
-*/
