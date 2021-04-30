f7d73ffec0826a109d9e3d9b73c15a6c7c8c5899
Oznnniv <>
2021-04-29 19:17:33 -0500

    New version

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -2,7 +2,7 @@ export var GLOBAL = {
 	url: 'http://localhost:80/',
 	ip: '0.0.0.0'
 };
-/*
+/*VARIABLE lOCAL
 export var GLOBAL = {
 	url: 'http://host.docker.internal:5000/',
 	ip: 'host.docker.internal'
